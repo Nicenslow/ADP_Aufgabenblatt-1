1a6c7783112b3c48f4afa96f51c985654a93aa5f
Nicenslow <daniel.biedermann92@googlemail.com>
2018-04-10 12:18:06 +0200

    Stack done. ( Missing Commentary) / Missing Delete funktion (end / rnd + Commentary + Gemittelte Delete)

---
+++ Algorithmen und Datenstrukturen/src/aufgabenblatt_1/Performance.java
@@ -0,0 +1,212 @@
+package aufgabenblatt_1;
+
+import java.util.Random;
+
+public class Performance {
+	/**
+	 * Die Klasse PerformanceTest fügt jeweils Elemente zu verschiedenen
+	 * Listenformen hinzu und vergleicht die Performance beider.
+	 * 
+	 * @author kate
+	 *
+	 *
+	 *         /** einfach verlinkte Liste als Objektvariable
+	 */
+	private LineareArrayListe<Integer> al;
+	/**
+	 * doppelt verloinkte Liste als Objektvariable
+	 */
+	private DoppeltVerketteteListe<Integer> dl;
+
+	/**
+	 * Konstruktor
+	 * 
+	 * @param l1
+	 * @param l2
+	 */
+	public Performance(LineareArrayListe<Integer> l1, DoppeltVerketteteListe<Integer> l2) {
+		this.al = l1;
+		this.dl = l2;
+	}
+
+	// Hilfmethode der einfach verlinkten Liste um an den Anfang Elemente
+	// hinzuzufügen
+	private long anfangsEinfuegenAL() {
+		al.clearList();
+		long time1 = System.currentTimeMillis();
+		for (int i = 0; i <= 10000; i++) {
+			int temp = new Random().nextInt();
+			al.insertElement(0, temp);
+		}
+		long timeFinalL1 = System.currentTimeMillis() - time1;
+		return timeFinalL1;
+	}
+
+	// Hilfmethode der doppelt verketteten Liste um an den Anfang Elemente
+	// hinzuzufügen
+	private long anfangsEinfuegenDL() {
+		dl.clearList();
+		long time2 = System.currentTimeMillis();
+		for (int i = 0; i <= 10000; i++) {
+			int temp = new Random().nextInt();
+			dl.insertElement(0, temp);
+		}
+		long timeFinalL2 = System.currentTimeMillis() - time2;
+		return timeFinalL2;
+	}
+
+	/**
+	 * Hier werden jeweils 10.000 Elemente an den Anfang einer einfach und einer
+	 * doppelt verketteten Liste gefügt und die Dauer, die dafür nötig war, wird
+	 * aus der Konsole ausgegeben.
+	 * 
+	 * @return true
+	 */
+	public boolean insertAtStart() {
+		System.out.println(
+				"10000 Elemente anfangs einfügen in der ArrayList dauert " + anfangsEinfuegenAL() + " Millisekunden");
+		System.out.println("10000 Elemente anfangs einfügen in der Doppelt verlinkten List dauert "
+				+ anfangsEinfuegenDL() + " Millisekunden");
+		System.out.println();
+		return true;
+	}
+
+	// Hilfsmethode zum zufälligen Einfügen von Elementen in die einfach
+	// verkettete Liste
+	private long randomEinfuegenAL() {
+		al.clearList();
+		long time1 = System.currentTimeMillis();
+		for (int i = 0; i <= 10000; i++) {
+			int temp = new Random().nextInt();
+			al.insertElement((int) (Math.random() * al.getCount()), temp);
+		}
+		long timeFinalL1 = System.currentTimeMillis() - time1;
+		return timeFinalL1;
+	}
+
+	// Hilfsmethode zum zufälligen Einfügen von Elementen in die doppelt
+	// verkettete Liste
+	private long randomEinfuegenDL() {
+		dl.clearList();
+		long time2 = System.currentTimeMillis();
+		for (int i = 0; i <= 10000; i++) {
+			int temp = new Random().nextInt();
+			dl.insertElement((int) (Math.random() * dl.getCount()), temp);
+		}
+		long timeFinalL2 = System.currentTimeMillis() - time2;
+		return timeFinalL2;
+	}
+
+	/**
+	 * Diese Methode fügt jeweils 10.000 elemente zufällig in einer einfach und
+	 * in einer doppelt verketteten Liste ein und gibt die Dauer, die jeweils
+	 * dafür benötigt wurde, auf der Konsole aus.
+	 * 
+	 * @return boolean
+	 */
+	public boolean insertRandomly() {
+		System.out.println(
+				"10000 Elemente zufällig einfügen in der ArrayList dauert " + randomEinfuegenAL() + " Millisekunden");
+		System.out.println("10000 Elemente zufällig einfügen in der Doppelt verlinkten List dauert "
+				+ randomEinfuegenDL() + " Millisekunden");
+		System.out.println();
+		return true;
+	}
+
+	// Hilfsmethode, die am Ende einer einfach verketteten Liste 10.000 Elemente
+	// einfügt
+	private long endeEinfuegenAL() {
+		al.clearList();
+		long time1 = System.currentTimeMillis();
+		for (int i = 0; i <= 10000; i++) {
+			int temp = new Random().nextInt();
+			al.insertElement(al.getCount(), temp);
+		}
+		long timeFinalL1 = System.currentTimeMillis() - time1;
+		return timeFinalL1;
+	}
+
+	// Hilfsmethode, die am Ende einer doppelt verketteten Liste 10.000 Elemente
+	// einfügt
+	public long endeEinfuegenDL() {
+		dl.clearList();
+		long time2 = System.currentTimeMillis();
+		for (int i = 0; i <= 10000; i++) {
+			int temp = new Random().nextInt();
+			dl.insertElement(dl.getCount(), temp);
+		}
+		long timeFinalL2 = System.currentTimeMillis() - time2;
+		return timeFinalL2;
+	}
+
+	/**
+	 * Diese Methode fügt jeweils 10.000 Elemente am Ende in einer einfach und
+	 * in einer doppelt verketteten Liste ein und gibt die Dauer, die jeweils
+	 * dafür benötigt wurde, auf der Konsole aus.
+	 * 
+	 * @return boolean
+	 */
+	public boolean InsertAtEnd() {
+		System.out.println(
+				"10000 Elemente am Ende einfügen in der ArrayList dauert " + endeEinfuegenAL() + " Millisekunden");
+		System.out.println("10000 Elemente am Ende einfügen in der Doppelt verlinkten Liste dauert " + endeEinfuegenDL()
+				+ " Millisekunden");
+		System.out.println();
+		return true;
+	}
+
+	public void mitteln() {
+		long anfangAL = 0;
+		long anfangDL = 0;
+		long randomAL = 0;
+		long randomDL = 0;
+		long endAL = 0;
+		long endDL = 0;
+		for (int i = 0; i < 10; i++) {
+			anfangAL += anfangsEinfuegenAL();
+			anfangDL += anfangsEinfuegenDL();
+			randomAL += randomEinfuegenAL();
+			randomDL += randomEinfuegenDL();
+			endAL += endeEinfuegenAL();
+			endDL += endeEinfuegenDL();
+		}
+		anfangAL /= 10;
+		anfangDL /= 10;
+		randomAL /= 10;
+		randomDL /= 10;
+		endAL /= 10;
+		endDL /= 10;
+		System.out.println("Der Mittelwert für das Einfuegen am Anfang einer einfachen Liste ist: " + anfangAL);
+		System.out.println("Der Mittelwert für das Einfuegen am Anfang doppelt verketteten Liste ist: " + anfangDL);
+		System.out.println("Der Mittelwert für das zufällige Einfuegen in einer einfachen Liste ist: " + randomAL);
+		System.out.println(
+				"Der Mittelwert für das zufällige Einfuegen in einer doppelt verketteten Liste ist: " + randomDL);
+		System.out.println("Der Mittelwert für das Einfuegen am Ende in einer einfachen Liste ist: " + endAL);
+		System.out.println("Der Mittelwert für das Einfuegen am Ende in einer doppelt verketteten Liste ist: " + endDL);
+	}
+
+	public long anfangsLoeschenAL() {
+		long timeArrayList = System.currentTimeMillis();
+		for (int i = 0; i <= 1000; i++) {
+			al.deleteElement(0);
+		}
+		return timeArrayList = System.currentTimeMillis() - timeArrayList;
+	}
+	
+	public long anfangsLoeschenDL() {
+		long timeArrayList = System.currentTimeMillis();
+		for (int i = 0; i <= 1000; i++) {
+			dl.deleteElement(0);
+		}
+		return timeArrayList = System.currentTimeMillis() - timeArrayList;
+	}
+
+
+	public static void main(String[] args) {
+		Performance p1 = new Performance(new LineareArrayListe<Integer>(), new DoppeltVerketteteListe<Integer>());
+		// p1.anfangsEinfuegen();
+		// p1.randomEinfuegen();
+		// p1.endeEinfuegen();
+		p1.mitteln();
+	}
+}
--- Algorithmen und Datenstrukturen/src/aufgabenblatt_1/Stackrechnung.java
+++ Algorithmen und Datenstrukturen/src/aufgabenblatt_1/Stackrechnung.java
@@ -1,26 +1,87 @@
 package aufgabenblatt_1;
-
+/**
+ * Die Klasse Stackrechnung 
+ * @author wobi2016
+ *
+ */
 public class Stackrechnung {
 
-	Stack stack;
+	Stack<Integer> stack;
 
-	public Stackrechnung(Stack s) {
+	public Stackrechnung(Stack<Integer> s) {
 		this.stack = s;
 	}
 
-	public void input(String str) {
+	public int berechnung(String str) {
 		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) > 39 && str.charAt(i) < 48 && (str.charAt(i) != 44 || str.charAt(i) != 46)) {
-				stack.push(str.charAt(i));
-			} else {
+			int zeichen = (int) str.charAt(i);
+			if ((zeichen > 39 && zeichen < 58 && (zeichen != 44 && zeichen != 46))) {
+				if (zeichen == 41 && !stack.isEmpty()) {
+					int operand1 = getoprand(stack);
+					int operation = stack.pop();
+					int operand2 = getoprand(stack);
+					int dummy = stack.pop();
+					switch (operation) {
+					case 43:
+						stack.push(addition(operand1, operand2) + 48);
+						break;
+					case 42:
+						stack.push(multi(operand1, operand2) + 48);
+						break;
+					case 45:
+						stack.push(sub(operand1, operand2) + 48);
+						break;
+					case 47:
+						stack.push(div(operand1, operand2) + 48);
+						break;
+					}
+				} else {
+					stack.push(zeichen);
+				}
+			} else if (zeichen != 32) {
 				throw new IllegalArgumentException("ungülitges Zeichen entdeckt");
 			}
 		}
+		return (stack.pop() - 48);
 	}
 
-	public void addition() {
-		int operand1 = stack.pop();
+	public int getoprand(Stack<Integer> s) {
+		int operand = s.pop() - 48;
+		int multi = 10;
+		while (s.top() >= 48 && s.top() <= 57) {
+			operand += (s.pop() - 48) * multi;
+			multi *= 10;
+		}
+		return operand;
+	}
 
+	public int addition(int a, int b) {
+		return a + b;
+	}
+
+	public int multi(int a, int b) {
+		return a * b;
+	}
+
+	public int sub(int a, int b) {
+		return b - a;
+	}
+
+	public int div(int a, int b) {
+		if (a == 0) {
+			throw new IllegalArgumentException("durch 0 teilen nicht möglich");
+		}
+		return b / a;
+	}
+
+	public static void main(String[] args) {
+		Stack<Integer> stack1 = new Stack<>();
+		Stackrechnung stackrechnung = new Stackrechnung(stack1);
+		System.out.println(stackrechnung.berechnung("((2*4) + (8-4))"));
+		System.out.println(stackrechnung.berechnung("(((3-4) * (2-5)) +  (34 + 70))"));
+		System.out.println(stackrechnung.berechnung("(((5+3) / (4-2)) +  (67 - 13))"));
+		System.out.println(stackrechnung.berechnung("(((3-4) * (2-5)) +  (34 + 70))"));
+		System.out.println(stackrechnung.berechnung("(((12 - 18) * (3 + 7)) -  (3 - 4))"));
 	}
 
 }
